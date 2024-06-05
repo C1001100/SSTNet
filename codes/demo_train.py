953ecd5081d25393787d9a350f466fa76d893786
C <45863430+C1001100@users.noreply.github.com>
2024-06-05 21:38:21 +0800

    Update

--- codes/demo_train.py
+++ codes/demo_train.py
@@ -17,7 +17,6 @@ if __name__ == '__main__':
 
 
     checkpoint = utils.checkpoint(args)
-    # args.pre_train = '../experiment/model/model_best.pt'
 
     if checkpoint.ok:
         dataloaders = data.create_dataloaders(args)
@@ -26,13 +25,6 @@ if __name__ == '__main__':
         sr_loss = loss.Loss(args, checkpoint) if not args.test_only else None
         t = trainer.Trainer(args, dataloaders, sr_model, sr_loss, checkpoint)
 
-        # print("Model's state_dict:")
-        # for param_tensor in sr_model.state_dict():
-        #                 checkpoint.write_log(
-        #         '[param_tensor {}]\tmodel.state_dict: {}'.format(param_tensor, sr_model.state_dict()[param_tensor].size())
-        #     )
-
-        #checkpoint.write_log('now{}'.format(sr_model))
 
         while not t.terminate():
             t.train()
