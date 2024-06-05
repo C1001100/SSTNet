6d80312c2c250145134540ee484a8d8c0c18cf80
C <45863430+Matrix-Cc@users.noreply.github.com>
2024-06-05 12:23:48 +0800

    Update

--- codes/data/__init__.py
+++ codes/data/__init__.py
@@ -5,23 +5,22 @@ def create_dataloaders(args):
     """create dataloader"""
     if args.dataset == 'AID':
         from data.aid import AIDataset
-        training_set = AIDataset(args, root_dir='/home/featurize/data/AID-dataset/train',  # AID_dataset
+        training_set = AIDataset(args, root_dir='/home/data/AID-dataset/train',  # AID_dataset
                                  train=True)
-        #E:/leisen-workspace/codelife/super-resolution/dataset/SR for remote sensing/AID_dataset
-        #D:\A_DataSet\AID-dataset\train
-        val_set = AIDataset(args, root_dir='/home/featurize/data/AID-dataset/val',
+        
+        val_set = AIDataset(args, root_dir='/home/data/AID-dataset/val',
                             train=False)
     elif args.dataset == 'UCMerced':
         from data.ucmerced import UCMercedDataset
-        training_set = UCMercedDataset(args, root_dir='/home/featurize/data//UCMerced-dataset/train/',
+        training_set = UCMercedDataset(args, root_dir='/home/data//UCMerced-dataset/train/',
                                  train=True)
-        val_set = UCMercedDataset(args, root_dir='/home/featurize/data//UCMerced-dataset/val/',
+        val_set = UCMercedDataset(args, root_dir='/home/data//UCMerced-dataset/val/',
                             train=False)
     elif args.dataset == 'DIV2K':
         from data.div2k import DIV2KDataset
-        training_set = DIV2KDataset(args, root_dir='D:/Atemp/SR for natural image/DIV2K_train',
+        training_set = DIV2KDataset(args, root_dir='/Atemp/SR for natural image/DIV2K_train',
                                     train=True)
-        val_set = DIV2KDataset(args, root_dir='D:/Atemp/SR for natural image/valid',
+        val_set = DIV2KDataset(args, root_dir='/Atemp/SR for natural image/valid',
                                train=False)
     else:
         raise NotImplementedError(
